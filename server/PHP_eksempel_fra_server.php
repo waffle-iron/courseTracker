9905aa73c62922b208f1f1cd3caf4cfc5d6940ae
Peter Bulukin <pc.bulukin@gmail.com>
2017-03-03 16:42:48 +0100

    PHP exampel to look at added

---
+++ server/PHP_eksempel_fra_server.php
@@ -0,0 +1,30 @@
+<?php
+$servername = "138.197.33.171";
+$dbname     = "coursetracker";
+$username   = "coursetracker";
+$password   = "admin";
+
+//$coursecode = $_GET['courseID'];
+
+// Create connection
+$connection = mysqli_connect($servername, $username, $password, $dbname)
+  or die("Error " . mysqli_error($connection));
+
+/*$sql = "select TIME_FORMAT(time, '%H:%i'), courses.courseID, courseName, location
+        from courses
+        inner join lecture
+        WHERE courses.courseID = '$coursecode'
+        AND courses.courseID = lecture.courseID";
+*/
+
+$sql = "select courses.courseID from courses";
+$result = mysqli_query($connection, $sql)
+  or die("Error in selecting " . mysqli_error($connection));
+
+$emparray = array();
+while ($row = mysqli_fetch_assoc($result)) {
+  $emparray[] = $row;
+}
+echo json_encode($emparray);
+mysqli_close($connection);
+?>
