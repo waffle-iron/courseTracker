522a062aefd4046d9d28322a0778bee89fe0c4fd
Molly <malfridha@gmail.com>
2017-03-09 13:52:49 +0100

    Error handling for user input

--- CourseTracker/app/src/main/java/gruppe087/coursetracker/LoginActivity.java
+++ CourseTracker/app/src/main/java/gruppe087/coursetracker/LoginActivity.java
@@ -3,27 +3,63 @@ package gruppe087.coursetracker;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.app.Activity;
+import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
+
+    LoginDataBaseAdapter loginDataBaseAdapter;
+    EditText editTextUserName,editTextPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        // create a instance of SQLite Database
+        loginDataBaseAdapter=new LoginDataBaseAdapter(this);
+        loginDataBaseAdapter=loginDataBaseAdapter.open();
+
+        // Get Refferences of Views (OBS SJEKK OM RIKTIG ID)
+        editTextUserName=(EditText)findViewById(R.id.editTextUserName);
+        editTextPassword=(EditText)findViewById(R.id.editTextPassword);
+
         // When Log in button pushed
         final Button button = (Button) findViewById(R.id.buttonSignIn);
         button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v){
+                // get The User name and Password
+                String userName=editTextUserName.getText().toString();
+                String password=editTextPassword.getText().toString();
+                // fetch the Password form database for respective user name
+                String storedPassword=loginDataBaseAdapter.getSinlgeEntry(userName);
+                // check if the Stored password matches with  Password entered by user
+                if(password.equals(storedPassword))
+                {
+                    Toast.makeText(LoginActivity.this, "Congrats: Login Successfull", Toast.LENGTH_LONG).show();
+                    return;
+                }
+                else
+                {
+                    Toast.makeText(LoginActivity.this, "User Name or Password does not match", Toast.LENGTH_LONG).show();
+                }
                 // Define action on click
                 Intent myIntent = new Intent(LoginActivity.this, MainActivity.class);
                 //Optional parameters: myIntent.putExtra("key", value);
                 LoginActivity.this.startActivity(myIntent);
             }
         });
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        // Close The Database
+        loginDataBaseAdapter.close();
     }
 
 
--- CourseTracker/app/src/main/java/gruppe087/coursetracker/SignupActivity.java
+++ CourseTracker/app/src/main/java/gruppe087/coursetracker/SignupActivity.java
@@ -12,20 +12,64 @@ import android.widget.Toast;
 
 public class SignupActivity extends AppCompatActivity {
 
+    EditText editTextUserName,editTextPassword,editTextConfirmPassword;
+    LoginDataBaseAdapter loginDataBaseAdapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
 
+        // get Instance  of Database Adapter
+        loginDataBaseAdapter=new LoginDataBaseAdapter(this);
+        loginDataBaseAdapter=loginDataBaseAdapter.open();
+
+        // Get Refferences of Views
+        editTextUserName=(EditText)findViewById(R.id.editTextUserName);
+        editTextPassword=(EditText)findViewById(R.id.editTextPassword);
+        editTextConfirmPassword=(EditText)findViewById(R.id.editTextConfirmPassword);
+
         // When Sign up button pushed
         final Button button = (Button) findViewById(R.id.buttonCreateAccount);
         button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v){
+
+                String userName=editTextUserName.getText().toString();
+                String password=editTextPassword.getText().toString();
+                String confirmPassword=editTextConfirmPassword.getText().toString();
+
+                // check if any of the fields are vaccant
+                if(userName.equals("")||password.equals("")||confirmPassword.equals(""))
+                {
+                    Toast.makeText(getApplicationContext(), "Field Vaccant", Toast.LENGTH_LONG).show();
+                    return;
+                }
+                // check if both password matches
+                if(!password.equals(confirmPassword))
+                {
+                    Toast.makeText(getApplicationContext(), "Password does not match", Toast.LENGTH_LONG).show();
+                    return;
+                }
+                else
+                {
+                    // Save the Data in Database
+                    loginDataBaseAdapter.insertEntry(userName, password);
+                    Toast.makeText(getApplicationContext(), "Account Successfully Created ", Toast.LENGTH_LONG).show();
+                }
+
                 // Define action on click
                 Intent myIntent = new Intent(SignupActivity.this, ChooseCourseAtSetupActivity.class);
                 //Optional parameters: myIntent.putExtra("key", value);
                 SignupActivity.this.startActivity(myIntent);
             }
         });
+    }
+
+    @Override
+    protected void onDestroy() {
+        // TODO Auto-generated method stub
+        super.onDestroy();
+
+        loginDataBaseAdapter.close();
     }
 }
