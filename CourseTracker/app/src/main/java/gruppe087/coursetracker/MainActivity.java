99b86db96c5a9de2cfcef3bc289dc5d6213a7919
Molly <malfridha@gmail.com>
2017-03-14 13:24:32 +0100

    Added "Course Tracker" as title in main activity

--- CourseTracker/app/src/main/java/gruppe087/coursetracker/MainActivity.java
+++ CourseTracker/app/src/main/java/gruppe087/coursetracker/MainActivity.java
@@ -39,6 +39,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("Course Tracker");
 
         TabLayout tabLayout = (TabLayout) findViewById(R.id.tab_layout);
         tabLayout.addTab(tabLayout.newTab().setText("Today's lectures"));
