954903b3fef55ee4d35d57c140acd61d34d2ad12
Molly <malfridha@gmail.com>
2017-03-20 10:08:38 +0100

    Created test files for the setup

---
+++ CourseTracker/app/src/test/java/gruppe087/coursetracker/LoginDataBaseAdapterTest.java
@@ -0,0 +1,58 @@
+package gruppe087.coursetracker;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by malfridhendenaaraas on 20.03.2017.
+ */
+public class LoginDataBaseAdapterTest {
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void open() throws Exception {
+
+    }
+
+    @Test
+    public void close() throws Exception {
+
+    }
+
+    @Test
+    public void getDatabaseInstance() throws Exception {
+
+    }
+
+    @Test
+    public void insertEntry() throws Exception {
+
+    }
+
+    @Test
+    public void deleteEntry() throws Exception {
+
+    }
+
+    @Test
+    public void getSinlgeEntry() throws Exception {
+
+    }
+
+    @Test
+    public void updateEntry() throws Exception {
+
+    }
+
+}
